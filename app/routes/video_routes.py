5cfddad976fabd8fcb320e9d38ae3dcf6455b18b
Uche Ofia <coolucheofia@gmail.com>
2023-10-11 18:47:10 +0100

    refactor: Remove dynamic parameter from route

--- app/routes/video_routes.py
+++ app/routes/video_routes.py
@@ -352,7 +352,7 @@ def update_title(video_id: str, title: str, db: Session = Depends(get_db)):
     return {"msg": "Title updated successfully!"}
 
 
-@router.patch("/videos/transfer/from-{username1}/to-{username2}")
+@router.patch("/videos/transfer/")
 def transfer_videos(
     username1: str, username2: str, db: Session = Depends(get_db)
 ):
