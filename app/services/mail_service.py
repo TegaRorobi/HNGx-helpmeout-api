c6da01f416e220a667793a0c474cb2945e7b203f
DestinedCodes <yiradesat@gmail.com>
2023-10-13 14:34:52 +0100

    email temolate tested

--- app/services/mail_service.py
+++ app/services/mail_service.py
@@ -1,6 +1,7 @@
 """ This module contains the functions for sending emails to users. """
 import smtplib, ssl
 from mjml import mjml_to_html
+import pystache
 from email.message import EmailMessage
 from app.settings import (
     EMAIL_ADDRESS,
@@ -9,7 +10,7 @@ from app.settings import (
     EMAIL_PORT
 )
 
-def send_video(video_id: str, recepient_address: str):
+def send_video(username: str, video_id: str, recepient_address: str):
     """
     Sends an email to the user with the video embedded in the email.
     
@@ -26,10 +27,20 @@ def send_video(video_id: str, recepient_address: str):
     msg["From"] = EMAIL_ADDRESS
     msg["To"] = recepient_address
 
+    params = {'username': username, 'video_id': video_id}
+    
     with open(f"app/services/video_mail.mjml", "rb") as f:
         mail = mjml_to_html(f)
 
-    msg.set_content(mail.html, subtype="html")
+    mail = mail.html
+    context={
+        'username': username,
+        'video_id': video_id,
+    }
+    mail = pystache.render(mail, context)
+
+
+    msg.set_content(mail, subtype="html")
 
     with smtplib.SMTP(EMAIL_HOST, EMAIL_PORT) as smtp:
         smtp.starttls(context=ssl.create_default_context())
