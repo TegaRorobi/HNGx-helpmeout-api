cc8bfdf8e53233374e1708ebcab7d08c6ddca57f
DestinedCodes <yiradesat@gmail.com>
2023-10-14 22:24:05 +0100

    add forgot oassword funxtion and emoty mjml file

--- app/services/mail_service.py
+++ app/services/mail_service.py
@@ -46,3 +46,38 @@ def send_video(username: str, video_id: str, recepient_address: str):
         smtp.starttls(context=ssl.create_default_context())
         smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
         smtp.send_message(msg)
+
+def send_otp(username: str, video_id: str, recepient_address: str):
+    """
+    Sends an email to the user with the video embedded in the email.
+    
+    Parameters:
+        video_id (str): The id of the video to be sent to the user.
+        recepient_address (str): The email address of the user.
+    
+    Returns:
+        message (str): A message indicating whether the email was sent
+            successfully.
+    """
+    msg = EmailMessage()
+    msg["Subject"] = "HelpMeOut Screen Recorder"
+    msg["From"] = EMAIL_ADDRESS
+    msg["To"] = recepient_address
+
+    with open(f"app/services/forgot_password_mail.mjml", "rb") as f:
+        mail = mjml_to_html(f)
+
+    mail = mail.html
+    context={
+        'email': email,
+        'otp': otp,
+    }
+    mail = pystache.render(mail, context)
+
+
+    msg.set_content(mail, subtype="html")
+
+    with smtplib.SMTP(EMAIL_HOST, EMAIL_PORT) as smtp:
+        smtp.starttls(context=ssl.create_default_context())
+        smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+        smtp.send_message(msg)
